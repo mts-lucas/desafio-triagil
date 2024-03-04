def49a6b73a3521080f5f6d398a0d7bfa9c2b676
Lucas Mateus <lmateus1067@outlook.com>
2024-03-04 19:39:39 -0300

    feat: add serializers

---
+++ api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from core.models import *
+
+class PokemonSerializer(serializers.ModelSerializer):
+    
+    id = serializers.IntegerField(source='dex_id')
+    class Meta:
+        model = Pokemon
+        fields = ['id', 'name', 'weight ', 'weight ']
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "weight": {"read_only": True},
+            "height": {"read_only": True}
+        }
+
+class TeamSerializer(serializers.ModelSerializer):
+
+    
+    class Meta:
+        model = Team
+        fields = ['owner', 'pokemons']
+        extra_kwargs = {
+            "owner": {"read_only": True},
+            "pokemons": {"read_only": True},
+        }
+
+# class TeamPostSerializer(serializers.ModelSerializer):
+
